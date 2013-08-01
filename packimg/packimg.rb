be62542bfc2f44a59134340da651a722b1f7f8d4
Tan Tran <ttran@gamesalad.com>
2013-08-01 12:40:22 -0500

    LE-155 Fix to allow a URL structure that's compatible with the mint bar.

--- packimg/packimg.rb
+++ packimg/packimg.rb
@@ -8,6 +8,32 @@ include Magick
 
 class PackImg < Sinatra::Base
 
+  get '/thumb/:resize/:format/:source' do
+
+    return "" unless params[:source]
+
+    # Encoding the source makes sure this url reads it properly.
+    source = Base64.decode64(params[:source]);
+
+    etag Digest::MD5.hexdigest("#{source}:#{params[:resize]}:#{params[:flip]}:#{params[:rotate]}:#{params[:format]}:#{params[:quality]}:#{params[:version]}")
+    img = Image.read(source).first
+
+    ops = []
+    ops << ->(image) { image }
+    ops << ->(image) { image.resize_to_fit(*params[:resize].split('x'))} if params[:resize]
+    ops << ->(image) { params[:flip] == 'vertical' ? image.flip : image.flop } if params[:flip]
+    ops << ->(image) { image.rotate(params[:rotate].to_i) } if params[:rotate]
+
+    res = ops.inject(img){|o,proc| proc.call(o)}
+
+    res.format = params[:format] if params[:format]
+    res.quality = params[:quality].to_i if params[:quality]
+
+    content_type res.mime_type
+
+    res.to_blob
+  end
+
   get '/' do
     
     return "" unless params[:source]
